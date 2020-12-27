3bac49c4d489daabb724b241c269a6a79ec75df5
Lupércio Fernando Ferraz <lupercio.ferraz@gmail.com>
2020-12-27 13:35:04 -0300

    create auth module

--- front-web/src/pages/Auth/components/Login/index.tsx
+++ front-web/src/pages/Auth/components/Login/index.tsx
@@ -1,17 +1,65 @@
+import ButtonIcon from 'core/components/ButtonIcon';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 import AuthCard from '../Card';
 import './styles.scss';
 
+type FormData = {
+    email: string;
+    password: string;
+}
+
 const Login = () => {
+
+    const { register, handleSubmit } = useForm<FormData>();
+
+    const onSubmit = (data: FormData) => {
+        console.log(data);
+    }
 
     return (
 
         <AuthCard title="login" >
             
-            <form className="login-form">
+            <form onSubmit={handleSubmit(onSubmit)} className="login-form">
 
-                <h1>Formulário Login</h1>
-            
+                <input 
+                    type="email"
+                    ref={register}
+                    name="email" 
+                    className="form-control input-base margin-botton-30" 
+                    placeholder="Email"
+                />
+
+                <input 
+                    type="password" 
+                    ref={register}
+                    name="password"
+                    className="form-control input-base" 
+                    placeholder="Senha"
+                />
+
+                <Link to="/admin/auth/recover" className="login-link-recover">
+                    Esqueci a senha?
+                </Link>
+
+                <div className="login-submit" >
+                    <ButtonIcon text="Logar" />
+                </div>
+
+                <div className="text-center">
+        
+                    <span className="not-registered">
+                        Não tem Cadastro?
+                    </span>
+        
+                    <Link to="/admin/auth/register" className="login-link-register">
+                        CADASTRAR
+                    </Link>
+
+                </div>
+
             </form>                        
 
         </AuthCard>
