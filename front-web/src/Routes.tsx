fa3d6d94fee6fd8130844d795ef9b1c0b98707dd
Lupércio Fernando Ferraz <lupercio.ferraz@gmail.com>
2021-01-03 16:00:09 -0300

    Routes Security

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -7,6 +7,7 @@ import Home from './pages/Home';
 import ProductDetails from './pages/Catalog/components/ProductDetails';
 import Auth from './pages/Auth';
 import history from './core/utils/history';
+import PrivateRoute from './core/components/Routes/PrivateRoute';
 
 const Routes = () => (
 
@@ -34,9 +35,9 @@ const Routes = () => (
             </Route>
 
             <Redirect from="/admin" to="/admin/products" exact />
-            <Route path="/admin">
+            <PrivateRoute path="/admin">
                 <Admin />
-            </Route>
+            </PrivateRoute>
 
         </Switch>
     </Router>
