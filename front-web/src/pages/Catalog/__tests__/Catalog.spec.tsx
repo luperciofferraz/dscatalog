8950924333d57bf0273a0dd27fa5e8a992d3a891
Lupércio Fernando Ferraz <lupercio.ferraz@gmail.com>
2021-04-07 10:13:48 -0300

    Products Lists Tests

---
+++ front-web/src/pages/Catalog/__tests__/Catalog.spec.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import Catalog from '..';
+import { Router } from 'react-router';
+import history from 'core/utils/history';
+import { productsResponse } from './fixtures';
+
+const server = setupServer(
+
+  rest.get('http://localhost:8080/products', (req, res, ctx) => {
+    return res(ctx.json(productsResponse))
+  })
+
+)
+  
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close()); 
+
+test('Should Render ProductCard', async () => {
+
+    //Arrange
+    
+    //Act
+    render(
+        <Router history={history}>
+            <Catalog />
+        </Router>
+    );
+
+    //Assert
+    expect(screen.getAllByTitle('Loading...')).toHaveLength(3);
+    expect(screen.getByText('Catálogo de Produtos')).toBeInTheDocument();
+
+    await waitFor(() => expect(screen.getByText('PC Gamer Alfa')).toBeInTheDocument());
+
+    expect(screen.getByText('PC Gamer')).toBeInTheDocument();
+    expect(screen.getByText('1')).toBeInTheDocument();
+    expect(screen.getByText('2')).toBeInTheDocument();
+    expect(screen.queryAllByTitle('Loading...')).toHaveLength(0);
+
+    screen.debug();
+});
